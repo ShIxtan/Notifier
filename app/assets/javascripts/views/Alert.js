3598c3c2084b6f85b644ec62576836154375535c
ShIxtan <aulbrich@iu.edu>
2015-04-05 21:24:46 -0700

    add alert queue

--- app/assets/javascripts/views/Alert.js
+++ app/assets/javascripts/views/Alert.js
@@ -2,11 +2,34 @@ Notifier.Views.Alert = Backbone.CompositeView.extend({
   template: JST['Alert'],
 
   initialize: function(){
-    this.listenTo(this.model, "sync", this.render);
+    this.listenTo(this.model, "sync", this.updateQueue.bind(this));
+    this.queue = [];
   },
 
   render: function(){
-    this.$el.html(this.template({content: this.model.content}));
+    this.$el.html(this.template({content: this.queue.shift()}));
+    if (this.queue){
+      this.renderIn(5000)
+    }
     return this
+  },
+
+  updateQueue: function(){
+    this.addToQueue(this.model.content);
+  },
+
+  addToQueue: function(string){
+    if (!this.queue){
+      this.renderIn(0);
+    }
+
+    this.queue.push(string);
+  },
+
+  renderIn: function(time){
+    var that = this
+    setTimeout(function(){
+      that.render()
+    }, time)
   }
 })
