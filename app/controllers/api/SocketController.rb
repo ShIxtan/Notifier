f796bfaa5d28204a3bff75f5cfbc0f47bf7337bf
ShIxtan <aulbrich@iu.edu>
2015-04-06 22:13:12 -0700

    add initial SocketController actions

--- app/controllers/api/SocketController.rb
+++ app/controllers/api/SocketController.rb
@@ -1,5 +1,41 @@
 class SocketController < WebsocketRails::BaseController
   def initialize_session
-    controller_store[:message_count] = 0
+  end
+
+  def new_message
+    if message[:content]
+      m = Message.create(message)
+      broadcast_message :new_message, message
+    end
+  end
+
+  def change_username
+    if message[:username]
+      current_username = connection_store[:user].username
+      if connection_store[:user].update_attributes username: message[:username]
+        new_message = "#{current_username} is now #{message[:username]}"
+        broadcast_message :new_message, new_message
+        broadcast_message :update_user, {username: message[:username], id: connection_store[:user].id}
+      end
+    end
+  end
+
+  def client_connected
+    @user = User.generate
+    connection_store[:user] = @user
+    new_message = "#{@user.username} has entered the room"
+    broadcast_message :new_message, new_message
+    brodcast_message :new_user, {username: @user.username, id: @user.id}
+    @user.messages.each do |m|
+      send_message :new_message, {content: m.content}
+    end
+  end
+
+  def client_disconnected
+    @user = connection_store[:user]
+    new_message = "#{@user.username} has left the room"
+    broadcast_message :new_message, new_message
+    broadcast_message :destroy_user, {username: @user.username, id: @user.id}
+    @user.destroy
   end
 end
--- config/events.rb
+++ config/events.rb
@@ -12,12 +12,11 @@ WebsocketRails::EventMap.describe do
   #   end
   # The above will handle an event triggered on the client like `product.new`.
   namespace :message do
-    subscribe :send, to: SocketController, with_method: send_message
+    subscribe :send, to: SocketController, with_method: new_message
   end
 
   subscribe :change_username, to: SocketController, with_method: change_username
 
-  subscribe :client_connected, to: SocketController, with_method: create_user
-  subscribe :client_disconnected, to: SocketController, with_method: destroy_user
-
+  subscribe :client_connected, to: SocketController, with_method: client_connected
+  subscribe :client_disconnected, to: SocketController, with_method: client_disconnected
 end
