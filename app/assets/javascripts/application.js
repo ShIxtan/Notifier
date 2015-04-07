f428dc986d01073d5c59829a0f54492080f91606
ShIxtan <aulbrich@iu.edu>
2015-04-06 21:34:05 -0700

    install websocket-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require underscore
 //= require backbone
+//= require websocket_rails/main
 //= require notifier
 //= require annyang.min.js
 //= require_tree ../templates
---
+++ app/controllers/api/SocketController.rb
@@ -0,0 +1,5 @@
+class SocketController < WebsocketRails::BaseController
+  def initialize_session
+    controller_store[:message_count] = 0
+  end
+end
